deee37fc9b4d81425bef023555e826121602c0b4
Sergey <pavbatol@yandex.ru>
2022-04-30 20:19:34 +0400

    Удалены модификаторы Private

--- Converter.java
+++ Converter.java
@@ -1,13 +1,9 @@
 public class Converter{
-    private static double rateUSD = 78.5;
-    private static double rateEUR = 88.7;
-    private static double rateJPY = 0.75;
+    double rateUSD = 78.5;
+    double rateEUR = 88.7;
+    double rateJPY = 0.75;
 
-    public static void main(String[] args){
-        convert(2000, 1);
-    }
-    
-    public static void convert(double rubles, int currency){
+    void convert(double rubles, int currency){
         if (currency == 1) {
             System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
         } else if (currency == 2) {
